7d9747d3c224299a9949de25ce118ceb0d2ede16
Victor FONTAINE <vifontai@e3r13p14.42.fr>
2021-04-14 11:16:16 +0200

    ready to correct norminette done

--- libasm.h
+++ libasm.h
@@ -1,3 +1,15 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   libasm.h                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: vifontai <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/04/14 11:12:36 by vifontai          #+#    #+#             */
+/*   Updated: 2021/04/14 11:15:50 by vifontai         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #ifndef LIBASM_H
 # define LIBASM_H
 
@@ -10,8 +22,8 @@
 size_t		ft_strlen(const char *str);
 ssize_t		ft_write(int fildes, const void *buf, size_t nbyte);
 ssize_t		ft_read(int fildes, void *buf, size_t nbyte);
-char		*ft_strcpy(char * dst, const char * src);
-int 		ft_strcmp(const char *s1, const char *s2);
+char		*ft_strcpy(char *dst, const char *src);
+int			ft_strcmp(const char *s1, const char *s2);
 char		*ft_strdup(const char *s1);
 
 #endif
